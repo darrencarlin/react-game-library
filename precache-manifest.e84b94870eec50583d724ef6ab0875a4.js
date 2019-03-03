80dfc08c703a4afaed7233fa24c06a90acbaad48
Darren Carlin <daz@Darrens-MacBook-Pro.local>
2019-03-02 23:39:56 -0500

    Updates

---
+++ precache-manifest.e84b94870eec50583d724ef6ab0875a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "21d6958ca3f92a9266eb",
+    "url": "/react-game-library/static/js/runtime~main.21d6958c.js"
+  },
+  {
+    "revision": "f0ae4fd64b416e0a0fc5",
+    "url": "/react-game-library/static/js/main.f0ae4fd6.chunk.js"
+  },
+  {
+    "revision": "48c39327cbbae9519d4b",
+    "url": "/react-game-library/static/js/1.48c39327.chunk.js"
+  },
+  {
+    "revision": "f0ae4fd64b416e0a0fc5",
+    "url": "/react-game-library/static/css/main.a5c8d3ec.chunk.css"
+  },
+  {
+    "revision": "27c59cf70a067fa917fbee1362945656",
+    "url": "/react-game-library/index.html"
+  }
+];
